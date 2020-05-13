86637df02105d568fdca98dcb1499c700e1b20b3
Renata-code <64097993+Renata-code@users.noreply.github.com>
2020-05-13 17:07:46 -0400

    Update rena_s_codes.java

--- src/day_1/rena_s_codes.java
+++ src/day_1/rena_s_codes.java
@@ -3,5 +3,8 @@ package day_1;
 public class rena_s_codes {
     public static void main(String[] args) {
         System.out.println("Git classes practice");
+        
+        
+        //doing some changes in my codes
     }
 }
