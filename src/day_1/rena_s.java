7ac24a214cc01f57be08632ec608bb3cf228e9fd
Renata Saidjanova <renatasaidjanova@Renatas-MBP.fios-router.home>
2020-05-13 17:01:30 -0400

    rena_s.java

---
+++ src/day_1/rena_s.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class rena_s {
+    public static void main(String[] args) {
+        System.out.println("Git Practicing");
+        System.out.println("Hello world");
+    }
+}
