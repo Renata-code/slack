936a0bddb994f144e506c178c56fd393127efe4a
Renata-code <64097993+Renata-code@users.noreply.github.com>
2020-05-13 16:51:53 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -5,5 +5,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class LogCall {
     public static void main(String[] args) {
         System.out.println("Users are able to call through the class ");
+        
+        //Updating from GitHub 
+        
     }
 }
