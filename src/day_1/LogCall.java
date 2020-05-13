2ad26d43b2ae46e2b3294ee6ee4c1aa1951139e0
Renata Saidjanova <renatasaidjanova@Renatas-MBP.fios-router.home>
2020-05-13 16:46:26 -0400

    LogCall.java

---
+++ src/day_1/LogCall.java
@@ -0,0 +1,9 @@
+package day_1;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class LogCall {
+    public static void main(String[] args) {
+        System.out.println("Users are able to call through the class ");
+    }
+}
