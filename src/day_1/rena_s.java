fc9ff6f974f93296f488c6fc4f12c40c46837cf6
Renata-code <64097993+Renata-code@users.noreply.github.com>
2020-05-13 17:03:29 -0400

    Update rena_s.java

--- src/day_1/rena_s.java
+++ src/day_1/rena_s.java
@@ -4,5 +4,9 @@ public class rena_s {
     public static void main(String[] args) {
         System.out.println("Git Practicing");
         System.out.println("Hello world");
+        
+        //Updating from github to Intellij
+        
+        
     }
 }
